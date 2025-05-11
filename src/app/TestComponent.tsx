49b7af9df6ef6c38505e6298390d7030126dd16e
Anastasiya Gurianova <gurianova.nast@gmail.com>
2025-05-11 17:11:55 +0300

    feat(widgets): создать Header

--- src/app/TestComponent.tsx
+++ src/app/TestComponent.tsx
@@ -1,35 +1,48 @@
-import { Button, Typography } from '@mui/material';
-import { useAppDispatch, useAppSelector } from './store/hooks';
-import { login, logout } from 'entities/user';
+import { Typography } from '@mui/material';
+import { useState } from 'react';
+import { Button, Input, Form, Modal, Card, AppWrapper } from 'shared/ui';
+import { Header } from 'widgets/Header';
 
 export const TestComponent = () => {
-	const dispatch = useAppDispatch();
-	const { isAuthenticated, user } = useAppSelector((state) => state.user);
+	const [open, setOpen] = useState(false);
+	//const [page, setPage] = useState(1);
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		console.log('Form submitted');
+	};
 
 	return (
-		<div>
-			<Typography variant="h1">Online Fitness Trainer</Typography>
-			<Typography>
-				Auth: {isAuthenticated ? `Logged in as ${user?.name}` : 'Not logged in'}
-			</Typography>
-			<Button
-				variant="contained"
-				color="primary"
-				onClick={() =>
-					dispatch(
-						login({ id: '1', name: 'Test User', email: 'test@example.com' }),
-					)
-				}
-			>
-				Login
-			</Button>
-			<Button
-				variant="outlined"
-				color="secondary"
-				onClick={() => dispatch(logout())}
-			>
-				Logout
-			</Button>
-		</div>
+		<>
+			<Header />
+			<AppWrapper>
+				<Card title="Test Card">
+					<Typography>Test content in card</Typography>
+				</Card>
+
+				<Form onSubmit={handleSubmit}>
+					<Input label="Name" />
+					<Input label="Password" type="password" />
+
+					<Button label="Submit" type="submit" />
+				</Form>
+
+				<Card title="">
+					<Button label="Open Modal" onClick={() => setOpen(true)} />
+				</Card>
+
+				<Modal
+					open={open}
+					onClose={() => setOpen(false)}
+					title="Test Modal"
+					onSubmit={() => {
+						console.log('Modal submitted');
+						setOpen(false);
+					}}
+				>
+					<Typography>Modal content</Typography>
+				</Modal>
+			</AppWrapper>
+		</>
 	);
 };
